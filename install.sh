4174bebafc083ec59293575c661eb34642734ce4
Sanket Desai <desai.sanket12@gmail.com>
2020-09-30 16:52:37 +0530

    Freebayes installation fix

--- install.sh
+++ install.sh
@@ -39,22 +39,24 @@ fi
 if [[ ! -f $home_folder/external/tabix/tabix  ]];
 then
   cd $home_folder/external
+  rm -rf tabix
   git clone --recursive https://github.com/samtools/tabix.git
   cd tabix && make && cd $home_folder
 fi
 
-if [[ ! -d $home_folder/external/htslib-1.10 ]];
+if [[ -d $home_folder/external/htslib-1.10 ]];
 then
 	cd $home_folder/external/
-	wget https://github.com/samtools/htslib/releases/download/1.10/htslib-1.10.tar.bz2
-	tar -xf htslib-1.10.tar.bz2
-	cd htslib-1.10
-	make
-	rm $home_folder/external/htslib-1.10.tar.bz2
-	mv $home_folder/external/htslib-1.10 $home_folder/external/htslib
+  rm -rf htslib
+  git clone --recursive https://github.com/samtools/htslib.git
+  cd htslib && ./configure && make
 	cd $home_folder
 else
-	cd $home_folder/external/htslib && make && cd $home_folder
+  cd $home_folder/external/
+  rm -rf htslib
+  git clone --recursive https://github.com/samtools/htslib.git
+  cd htslib && ./configure && make
+	cd $home_folder
 fi
 
 if [[ ! -d $home_folder/external/samtools-1.10 ]];
@@ -90,6 +92,7 @@ fi
 # bamtools for freebayes
 
 cd $home_folder/external/
+rm -rf bamtools
 git clone git://github.com/pezmaster31/bamtools.git
 cd bamtools
 mkdir build
@@ -99,21 +102,17 @@ make
 cd $home_folder
 #freebayes (freebayes requires g++, camke, the standard C and C++ development libraries, liblzma, pthread, and libbzip2.)
 
-if [[ ! -d "$home_folder/external/freebayes" ]];
+rm -rf $home_folder/external/freebayes
+git clone --recursive https://github.com/ekg/freebayes.git
+cd freebayes && make
+
+if [[ ! -f "$home_folder/external/freebayes/bin/freebayes" ]];
 then
-	cd $home_folder/external/
-	wget http://clavius.bc.edu/~erik/freebayes/freebayes-5d5b8ac0.tar.gz
-	tar xf freebayes-5d5b8ac0.tar.gz
-  mv bamtools ./freebayes
-	cd freebayes
-	if [ ! -f $home_folder/external/freebayes/bin/freebayes ];
-	then
-    make -j4
-		#echo "$SUDO_PASSWWORD" | sudo -S make && echo "$SUDO_PASSWWORD" | sudo -S make;
-	else
-		echo "Freebayes Installation Error !!!! Check the libraries."
-		abort
-	fi
+	echo "Freebayes installed successfully!!"
+else
+	echo "Freebayes Installation Error !!!! Check the libraries."
+	abort
+fi
 	cd $home_folder/external/
 	git clone --recursive https://github.com/vcflib/vcflib.git
 	cd $home_folder/external/vcflib
